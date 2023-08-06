4fac85a7c333ff5c25d2eaa813989e8abce1bca4
Bluemoontain <gbouvardgrange@live.fr>
2023-08-06 15:36:48 +0200

    IDs update

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+const ready = () => {
+  if (document.readyState !== 'loading') {
+    init();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+};
 
-const contentDiv = document.getElementById('content');
+const init = () => {
+  const myBooksDiv = document.getElementById('myBooks');
+  const newBookTitle = myBooksDiv.querySelector('h2');
 
-const addBookButton = document.createElement('button');
-addBookButton.textContent = 'Ajouter un livre';
+  const addBookButton = document.createElement('button');
+  addBookButton.textContent = 'Ajouter un livre';
+  addBookButton.addEventListener('click', showForm) //montrer form, cacher bouton;
+  newBookTitle.after(addBookButton);
 
-function showForm() {
-  
+  //showForm();
+
+  function showForm() {
     const form = document.createElement('form');
 
     const inputTitle = document.createElement('input');
     inputTitle.setAttribute('type', 'text');
     inputTitle.setAttribute('placeholder', 'Titre du livre');
+    inputTitle.id = 'inputTitle';
 
     const inputAuthor = document.createElement('input');
     inputAuthor.setAttribute('type', 'text');
@@ -20,22 +33,48 @@ function showForm() {
     submitButton.setAttribute('type', 'submit');
     submitButton.textContent = 'Enregistrer';
 
+    //cancelButton => reset form, cacher form, montrer addBookButton
+
     form.appendChild(inputTitle);
     form.appendChild(inputAuthor);
     form.appendChild(submitButton);
 
-    const nouveauLivreTitle = document.querySelector('.h2');
-    nouveauLivreTitle.insertAdjacentElement('afterend', form);
+    newBookTitle.after(form);
+    //form caché après init
 
-    addBookButton.disabled = true;
-    addBookButton.remove();
+    //addBookButton.disabled = true;
+    //addBookButton.remove();
+    
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+      
+        const inputs = form.querySelectorAll('input[type="text"]');
+
+        const titleValue2 = form.getElementById('inputTitle').value;
+      
+        const titleValue = inputs[0].value;
+        const authorValue = inputs[1].value;
+      
+        console.log('Titre du livre :', titleValue);
+        console.log('Auteur :', authorValue);
+      
+        form.reset();
+      });
 }
 
-addBookButton.addEventListener('click', showForm);
+// const nouveauLivreTitle = document.querySelector('.h2');
+// nouveauLivreTitle.insertAdjacentElement('afterend', addBookButton);
 
-const nouveauLivreTitle = document.querySelector('.h2');
-nouveauLivreTitle.insertAdjacentElement('afterend', addBookButton);
+const form = document.querySelector('form');
 
+}
+
+// TO DO
+// créer formulaire OK
 // placer correctement le formulaire (sous addbookbutton) OK
-// placer les elements ("titre du livre"/"auteur") les uns en dessous des autres ?
-// Afficher les mots clé saisis dans le formulaire dans la console au submit
+// placer les elements ("titre du livre"/"auteur") les uns en dessous des autres? CHECK
+// Afficher les mots clé saisis dans le formulaire dans la console au submit.  WIP Message erreur : Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')
+
+// ???
+// _ aucune modification du HTML possible ?
+//fetch https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
